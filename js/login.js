ae3b6c0215cd1dd16117b2c51c414b3b6845daff
EverD0 <47603859+EverD0@users.noreply.github.com>
2019-03-26 15:57:57 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,21 +1,3 @@
-/*
-var usuarios = {usuer:"Mestre", senha:"Everardo"};
-
-
-
-login(user_p,senha_p){
-
-  x = getElementById('user');
-  y = getElementById('senha');
-
-  if(x == usuarios.usuer && y == usuarios.senha){
-    window.location.href = "google.com";
-  }else{
-    getElementById('span').innerHTML = "Credenciais inválidas.</br> Verifique-as e tente novamente!";
-  }
-}
-*/
-//essa função da certo, só tem que saber como faz pra ela entrar direito no html
 function login(){
   var usuarios = {user:"Mestre", senha:"Everardo"};
   var x = document.getElementById('user');
